4c076a32119757075b844eab326e4b7b2776fa81
Emil410 <gasanovemil13@gmail.com>
2023-12-18 11:53:29 +0400

    JavaScript11

---
+++ assets/js/app.js
@@ -0,0 +1,27 @@
+// 1
+// let arr = [641, 5414, 486, 468, 5414, 412, 546];
+// console.log(arr.filter((fourtyOne) => fourtyOne.toString().includes("41")));
+// 2
+// let words = ["teyyareye minmisen? text, take, took ,telephone"];
+// let changeString = 0;
+// for (let i = 0; i < words.length; i++) {
+//     changeString = words.map(letter => letter.replace(/t/g, "*"));
+// }
+// console.log(changeString);
+// 4
+// let userNumber = +prompt("Kardesim bidene reqem yaz!");
+// let count = 0;
+// if (typeof(userNumber) === "number") {
+//     let eightNumber = parseInt(userNumber, 10) + 1;
+//     while (count < 4) {
+//         if (eightNumber % 10 === 8) {
+//             console.log(eightNumber);
+//             count++;
+//         }
+//         eightNumber++;
+//     }
+// } else {
+//     console.log("Reqem yaz eeee cumle yox");
+//}
+// 3
+// let number = +prompt("Kardesim bidene reqem yaz!");
